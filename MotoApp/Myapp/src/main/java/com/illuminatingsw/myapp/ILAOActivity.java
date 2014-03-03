355db67ad61838cfa0028aed501981417cbe7b6e
lesniakbj <lesniakbj@jay.washjeff.edu>
2014-03-03 16:07:48 -0500

    Fixed String throughout; added ILAO Activity and CARPLS Activity
    Further, added links to various pages to get to the new pages

---
+++ MotoApp/Myapp/src/main/java/com/illuminatingsw/myapp/ILAOActivity.java
@@ -0,0 +1,43 @@
+package com.illuminatingsw.myapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Brendan on 3/3/14.
+ */
+public class ILAOActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        Button buttonHome, backButton;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ilao_activity);
+
+        // HANDLE THE PRESSING OF THE Home BUTTON
+        buttonHome = (Button) findViewById(R.id.homebutton);
+        buttonHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Toast.makeText(getApplicationContext(), "FindHelp Button Pressed!", Toast.LENGTH_SHORT).show();
+
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+
+            };
+        } );
+
+        // HANDLE THE PRESSING OF THE Back BUTTON
+        buttonHome = (Button) findViewById(R.id.backbutton);
+        buttonHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Toast.makeText(getApplicationContext(), "FindHelp Button Pressed!", Toast.LENGTH_SHORT).show();
+
+                finish();
+
+            };
+        } );
+    }
+}
--- MotoApp/Myapp/src/main/java/com/illuminatingsw/myapp/InfoActivity.java
+++ MotoApp/Myapp/src/main/java/com/illuminatingsw/myapp/InfoActivity.java
@@ -3,8 +3,13 @@ package com.illuminatingsw.myapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class InfoActivity extends Activity {
@@ -26,5 +31,46 @@ public class InfoActivity extends Activity {
 
             };
         } );
+
+        // Link Carpls Page
+        String theString = getString(R.string.before_search2);
+        SpannableStringBuilder spannedString = new SpannableStringBuilder(theString);
+
+        ClickableSpan clickStringCarpls = new ClickableSpan()
+        {
+            public void onClick(View view)
+            {
+                startActivity(new Intent(InfoActivity.this, CarplsActivity.class));
+            }
+        };
+
+        spannedString.setSpan(clickStringCarpls, 5, 11, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setText(spannedString);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+
+        // Link ILAO Page
+        String theOtherString = getString(R.string.before_search3);
+        SpannableStringBuilder otherSpannedString = new SpannableStringBuilder(theOtherString);
+
+        ClickableSpan clickStringILAO = new ClickableSpan()
+        {
+            public void onClick(View view)
+            {
+                startActivity(new Intent(InfoActivity.this, ILAOActivity.class));
+            }
+        };
+
+        otherSpannedString.setSpan(clickStringILAO, 6, 30, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        TextView otherTextView = (TextView) findViewById(R.id.textView3);
+        otherTextView.setText(otherSpannedString);
+        otherTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+
     }
 }
