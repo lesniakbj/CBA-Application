163e6306489a32afbdc926a3fded04c9a6a39e83
lesniakbj <lesniakbj@jay.washjeff.edu>
2014-04-18 18:17:21 -0400

    Changed and added terms and conditions

--- MotoApp/Myapp/src/main/java/com/illuminatingsw/myapp/MainActivity.java
+++ MotoApp/Myapp/src/main/java/com/illuminatingsw/myapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.illuminatingsw.myapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -9,12 +12,44 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+    public static final String FIRST_RUN = "FirstRun";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Button buttonfindhelp, buttonabout, spanishbutton;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences settings = getSharedPreferences(FIRST_RUN, 0);
+        boolean isFirstRun = settings.getBoolean("firstRun", true);
+
+        if(isFirstRun)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Motorola Mobility Foundation Terms and Conditions");
+            builder.setMessage(R.string.info);
+
+            builder.setNeutralButton("I Agree", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int clicked) {
+                    SharedPreferences settings = getSharedPreferences(FIRST_RUN, 0);
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putBoolean("firstRun", false);
+                    editor.commit();
+                }
+            });
+
+            builder.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int clicked)
+                {
+                    Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                    homeIntent.addCategory(Intent.CATEGORY_HOME);
+                    homeIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(homeIntent);
+                }
+            });
+            builder.show();
+        }
 
         // HANDLE THE PRESSING OF THE Find Help BUTTON
         buttonfindhelp = (Button) findViewById(R.id.findhelpbutton);
